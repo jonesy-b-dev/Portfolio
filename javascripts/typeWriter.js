5331652f48c564affc20caf8ac46b4a2509c37b0
Jonas de Bruin <jonasdebruin05@gmail.com>
2024-03-09 15:27:08 +0100

    Added type writer effect to index text, also changed the text a bit

---
+++ javascripts/typeWriter.js
@@ -0,0 +1,13 @@
+var i = 0;
+var txt = 'Hi, I’m Jonas de Bruin(2005), a music producer and a software developer student from the Netherlands. \n I like to make desktop applications and games!';
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("mainText").innerHTML += txt.charAt(i);
+    i++;
+    var typingSpeed = Math.floor(Math.random() * 35) + 20; 
+    var delay = txt[i] === ' ' ? typingSpeed * 2 : typingSpeed;
+    setTimeout(typeWriter, delay);
+  }
+}
