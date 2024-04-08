7f7f240426dbcf8e509ed8fa82ecc5456f422f5e
Jonas de Bruin <jonasdebruin05@gmail.com>
2024-04-08 13:10:13 +0200

    Improved home text

--- javascripts/typeWriter.js
+++ javascripts/typeWriter.js
@@ -2,6 +2,10 @@ var i = 0;
 var txt = 'Hi, I’m Jonas de Bruin(2005), a music producer and a software developer student from the Netherlands. \n I like to make desktop applications, games and music!';
 var speed = 50;
 
+window.onload = () => {
+  typeWriter();
+}
+
 function typeWriter() {
   if (i < txt.length) {
     document.getElementById("mainText").innerHTML += txt.charAt(i);
