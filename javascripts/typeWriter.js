b186f440ddb2f874dbf67574efac8377749827a6
Jonas de Bruin <jonasdebruin@proton.me>
2024-11-26 15:38:08 +0100

    Adjust typewriter effect speed

--- javascripts/typeWriter.js
+++ javascripts/typeWriter.js
@@ -1,6 +1,6 @@
 var i = 0;
 var txt = 'Hi, I’m Jonas de Bruin(2005), a music producer (Jonesy B), part of the music duo The Rhythm Junctions and a software developer student from the Netherlands. \n I like to make desktop applications, games and music!';
-var speed = 50;
+var speed = 4;
 
 window.onload = () => {
   typeWriter();
@@ -10,7 +10,7 @@ function typeWriter() {
   if (i < txt.length) {
     document.getElementById("mainText").innerHTML += txt.charAt(i);
     i++;
-    var typingSpeed = Math.floor(Math.random() * 35) + 10; 
+    var typingSpeed = Math.floor(Math.random() * 35) + speed; 
     var delay = txt[i] === ' ' ? typingSpeed * 2 : typingSpeed;
     setTimeout(typeWriter, delay);
   }
