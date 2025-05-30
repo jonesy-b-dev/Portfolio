54b6790ee8f9eff4704502b5a2953bdb781796d2
Jonas de Bruin <jonasdebruin@proton.me>
2025-05-30 21:25:09 +0200

    Removed dublicate js code
    I was stupid back in the day

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,28 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Load navigation bar onto the page
-  fetch("../navbar.html")
+  fetch("/navbar.html")
       .then(response => response.text())
       .then(data => {
           document.getElementById("navbar-container").innerHTML = data;
       });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    // Load navigation bar onto the page
-    fetch("../../navbar.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("navbar-container-projectPages").innerHTML = data;
-        });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    // Load navigation bar onto the page
-    fetch("../navbar.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("navbar-container-otherPages").innerHTML = data;
-        });
 });
 
 //back button
