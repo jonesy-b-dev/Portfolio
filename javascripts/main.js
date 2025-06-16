f31625352d012991ead23a7cafc7f78fc5f26856
Jonas de Bruin <jonasdebruin@proton.me>
2025-06-16 15:19:22 +0200

    Organize some files

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Load navigation bar onto the page
-  fetch("/navbar.html")
+  fetch("/pages/partials/navbar.html")
       .then(response => response.text())
       .then(data => {
           document.getElementById("navbar-container").innerHTML = data;
